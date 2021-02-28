852842487c4c59d5e16af956ada84b8bee5f8364
LordMefloun <realfrog42@gmail.com>
2021-02-28 18:38:43 +0100

    Verze bez auth ALE FUNGUJE

--- app.js
+++ app.js
@@ -1,19 +1,40 @@
 var express = require('express');
+var url = require('url');
+const bodyParser = require('body-parser');
 var app = express();
-
-
-
 app.set('view engine', 'ejs');
+app.use(express.static("root"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+var mysql = require('mysql');
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "",
+  database: "zpravicky"
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+});
 
 
 
 app.get('/', function(req, res) {
-    res.render('pages/index');
+    con.query("SELECT * FROM clanek", function (err, result, fields) {
+        if (err) throw err;
+        res.render('pages/index', {clanky: result});
+      });
 });
+app.get('/clanky', function(req, res) {
+  con.query("SELECT * FROM clanek", function (err, result, fields) {
+      if (err) throw err;
+      var q = url.parse(req.url, true);
+      var q = q.search
 
-
-app.get('/about', function(req, res) {
-    res.render('pages/about');
+      res.render('pages/clanky', {clanky: result, search: q });
+    });
 });
 
 app.listen(3000);
